064ebfc122c3b0050ee281732280f6edc32cc981
Vijiraj <vijirajtp@gmail.com>
2025-07-31 15:21:34 +0530

    method to handle negative number and raise exception

--- calculator.rb
+++ calculator.rb
@@ -6,14 +6,23 @@ class Calculator
     # numbers.split(',').map { |n| n.strip.to_i }.sum
     
     sliced_numbers = numbers.gsub("\n", ',').split(',')
-    sliced_numbers.map do |number|
+    parsed_numbers = sliced_numbers.map do |number|
       stripped = number.strip.to_i
-    end.sum
+    end
+
+    negatives = parsed_numbers.select { |n| n < 0 }
+    raise ArgumentError, "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+
+    parsed_numbers.sum
   end
 end
+
+## Below examples to run and see the output
 
 calc = Calculator.new
 puts calc.add("")
 puts calc.add("1,2")
 puts calc.add("1 , 2 ")
 puts calc.add("1\n2,3")
+puts calc.add("1,2,-3")
+puts calc.add("-1,2,-3")
