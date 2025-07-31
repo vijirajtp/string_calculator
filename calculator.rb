0a2a41104b658d529dde7a099f7f7e72894bd70d
Vijiraj <vijirajtp@gmail.com>
2025-07-31 15:44:04 +0530

    Add the delimiter functionality in add method

--- calculator.rb
+++ calculator.rb
@@ -4,8 +4,17 @@ class Calculator
 
     # numbers.split(',').map(&:to_i).sum
     # numbers.split(',').map { |n| n.strip.to_i }.sum
+
+    delimiter = ','
+
+    if numbers.start_with?('//')
+      header, numbers = numbers.split("\n", 2)
+      delimiter = header[2..-1]
+    end
     
-    sliced_numbers = numbers.gsub("\n", ',').split(',')
+    # sliced_numbers = numbers.gsub("\n", ',').split(',')
+
+    sliced_numbers = numbers.split(/#{Regexp.escape(delimiter)}|\n/)
     parsed_numbers = sliced_numbers.map do |number|
       stripped = number.strip.to_i
     end
@@ -19,10 +28,13 @@ end
 
 ## Below examples to run and see the output
 
-# calc = Calculator.new
-# puts calc.add("")
-# puts calc.add("1,2")
-# puts calc.add("1 , 2 ")
-# puts calc.add("1\n2,3")
+calc = Calculator.new
+puts calc.add("")
+puts calc.add("1,2")
+puts calc.add("1 , 2 ")
+puts calc.add("1\n2,3")
 # puts calc.add("1,2,-3")
 # puts calc.add("-1,2,-3")
+puts calc.add("//;\n1;2")
+puts calc.add("//|\n5|8")
+puts calc.add("//-\n5-8")
